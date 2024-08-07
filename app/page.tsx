57484c52f6c1d6749507d9bed9bb7f16dd6495e4
Muhammed Mert Araz <67739721+measses@users.noreply.github.com>
2024-08-07 14:40:15 +0300

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="py-10 bg-mysage">
+      <h3>Mert Araz, NextJs Öğreniyorum!</h3>
+    </div>
+  );
+}
---
+++ tailwind.config.ts
@@ -0,0 +1,29 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      container: {
+        padding: "2rem",
+      },
+      colors: {
+        mysage: "#CCD5AE",
+        mybeige: "#E0E5B6",
+        mypastel: "#FAEDCE",
+        mylight: "#FEFAE0",
+      },
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
